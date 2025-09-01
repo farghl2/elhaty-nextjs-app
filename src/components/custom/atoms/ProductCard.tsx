e2d0065a56d070f2a2645abb43584fedc0ab416c
Islam <islammohammed2001@gmail.com>
2025-09-01 23:48:21 +0300

    add dialoge to product card

--- src/components/custom/atoms/ProductCard.tsx
+++ src/components/custom/atoms/ProductCard.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import { Card } from '@/components/ui/card'
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import useMenuType from '@/hooks/useMenuType'
 import { Plate } from '@/lib/types'
 
 
 
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 interface ProductCardProps {
     plate:Plate
@@ -15,8 +16,13 @@ interface ProductCardProps {
 
 export const ProductCard = ({plate}:ProductCardProps) => {
   const type = useMenuType();
-  
+  const [open, setOpen] = useState(false);
   return (
+    <>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        
+      
    <Card className='flex w-full flex-row-reverse px-2 py-1'>
     <Image 
     src={plate.imageUrl}
@@ -44,5 +50,31 @@ export const ProductCard = ({plate}:ProductCardProps) => {
       </div>
     </div>
    </Card>
+   </DialogTrigger>
+      <DialogContent className="sm:max-w-3xl flex flex-col items-center justify-center gap-6">
+        <DialogHeader>
+          <DialogTitle className='text-2xl'>{plate.title}</DialogTitle>
+        </DialogHeader>
+        <Image alt={plate.title} src={plate.imageUrl} width={500} 
+         height={500}
+         className='object-cover rounded-lg'
+         />
+         <h5 >{plate.desc}</h5>
+         <div className='flex items-center gap-8 justify-between'>
+       {plate.sizes.map((s,i)=>
+      <div key={i}>
+        <p className='mb-4'>{s.size}</p>
+        {type === "dinein" ? (
+                <p className='font-semibold text-lg'>{s.dineinPrice} ج.م</p>
+              ) : (
+                <p className='font-semibold text-lg'>{s.takeawayPrice} ج.م</p>
+              )}
+      </div>
+      )}
+      </div>
+        </DialogContent>
+        </Dialog>
+    </>
   )
 }
+
